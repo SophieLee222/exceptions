ccd84661825f1224387a4a6a5401d2b4f5863b2f
sofia va <sophia.lily.lee@gmail.com>
2024-10-29 23:05:07 +0300

    Shop Repository added

---
+++ src/test/java/ru/netology/ShopRepositoryTest.java
@@ -0,0 +1,30 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ShopRepositoryTest {
+
+    ShopRepository repo = new ShopRepository();
+    Product item1 = new Product(12, "book", 1000);
+
+    @Test
+    public void shouldRemoveExistingElement() {
+        repo.add(item1);
+        repo.remove(12);
+
+        Product expected = null;
+        Product actual = repo.findById(12);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldGenerateNotFoundException() {
+        repo.add(item1);
+
+        Assertions.assertThrows(NotFoundException.class, () -> {
+            repo.removeById(14);
+        });
+    }
+}
